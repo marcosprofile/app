c2346b1670d0ca0987063db68711fe8e3dfc854d
marcosprofile <marcos_laurindo1997@hotmail.com>
2024-02-20 22:19:09 -0300

    update tabs

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -4,12 +4,12 @@ export function Tabs() {
   return (
     <div className="border-b border-zinc-800 py-4">
       <nav className="flex items-center gap-2 max-w-[1200px] mx-auto">
-        <a href="" className="py-1.5 px-3 bg-zinc-800 text-zinc-100 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent">
+        <a href="" className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800">
           <ListVideo className="size-4" />
           Uploads
         </a>
-
-        <a href="" className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800">
+        
+        <a href="" className="py-1.5 px-3 bg-zinc-800 text-zinc-100 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent">
           <Tags className="size-4" />
           Tags
         </a>
